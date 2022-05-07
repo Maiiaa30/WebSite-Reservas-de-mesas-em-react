5391b839818f6f0165c8d1d9e73d348622fad8a8
Diogo Maia <diogomaia2003@gmail.com>
2022-05-07 22:55:21 +0100

    remove gallery thing

--- src/pages/Menu/index.tsx
+++ src/pages/Menu/index.tsx
@@ -1,18 +1,6 @@
 import Image from "next/image";
 import menu1 from "../../images/menu1.jpg";
 import menu2 from "../../images/menu2.jpg";
-import ImageGallery from "react-image-gallery";
-
-const images = [
-  {
-    original: { menu1 },
-    thumbnail: { menu1 },
-  },
-  {
-    original: { menu2 },
-    thumbnail: { menu2 },
-  },
-];
 
 export default function SignIn() {
   return (
@@ -20,7 +8,6 @@ export default function SignIn() {
       <div className="text-center mt-9">
         <Image src={menu1} priority alt="Menu1" />
         <Image src={menu2} priority alt="Menu2" />
-        <ImageGallery items={images} />
       </div>
     </div>
   );
