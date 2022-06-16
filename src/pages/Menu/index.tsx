6f7758ae7594137983958fce933a14cc7ca5c1f0
Diogo Maia <diogomaia2003@gmail.com>
2022-06-16 15:16:04 +0100

    Test Menu photos on mobile

--- src/pages/Menu/index.tsx
+++ src/pages/Menu/index.tsx
@@ -1,13 +1,22 @@
 import Image from "next/image";
 import menu1 from "../../images/menu1.jpg";
 import menu2 from "../../images/menu2.jpg";
+import Zoom from "react-medium-image-zoom";
+import "react-medium-image-zoom/dist/styles.css";
 
 export default function Menu() {
+  const photo_menu1 = require("../../images/menu1.jpg");
+  const photo_menu2 = require("../../images/menu2.jpg");
+
   return (
     <div>
       <div className="text-center mt-9">
-        <Image src={menu1} priority alt="Menu1" />
-        <Image src={menu2} priority alt="Menu2" />
+        <Zoom>
+          <Image alt="Menu1" src={photo_menu1} />
+        </Zoom>
+        <Zoom>
+          <Image alt="Menu2" src={photo_menu2} />
+        </Zoom>
       </div>
     </div>
   );
